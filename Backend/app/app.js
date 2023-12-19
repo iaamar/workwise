994d55175ae02f9c044b883c6064e8eebafe241d
Amar Nagargoje <amargithub@gmail.com>
2023-12-19 10:28:08 -0500

    added frontend origin

--- Backend/app/app.js
+++ Backend/app/app.js
@@ -7,14 +7,14 @@ import "dotenv/config";
 import router from "./routes/index.js";
 const corOptions = {
   credentials: true,
-  origin: "https://workwise-deploy-backend.onrender.com",
+  origin: "https://workwise-deploy-frontend.onrender.com/",
   methods: ["GET", "POST", "PUT", "DELETE"],
 };
 
 const initialize = (app) => {
   app.use(cors(corOptions)); // use cors middleware
   app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://workwise-deploy-backend.onrender.com");
+    res.header("Access-Control-Allow-Origin", "https://workwise-deploy-frontend.onrender.com/");
     res.header("Access-Control-Allow-Credentials", true);
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.header("Access-Control-Allow-Headers", "Content-Type");
